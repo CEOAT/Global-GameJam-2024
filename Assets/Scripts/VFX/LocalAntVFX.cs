79ed027a32422796d088e4b6246759a0193015a0
poomwarning <poomwarning08@gmail.com>
2024-01-27 23:39:17 +0700

    -add configuration random Stage

--- Assets/Scripts/VFX/LocalAntVFX.cs
+++ Assets/Scripts/VFX/LocalAntVFX.cs
@@ -1,9 +1,10 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using GGJ2024;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Random = System.Random;
 
 public class LocalAntVFX : MonoBehaviour
 {
@@ -11,9 +12,11 @@ public class LocalAntVFX : MonoBehaviour
   [SerializeField] private ParticleSystem Blood;
   [SerializeField] private SpriteRenderer _antVisual;
   private float TimeToClear;
+  private string[] animState = new[] {"Smoke","Smoke2" };
  
   private void Start()
   {
+   
     _smoke.enabled = false;
     _smoke.StopPlayback();
     _smoke.speed = 0;
@@ -29,7 +32,7 @@ public class LocalAntVFX : MonoBehaviour
   {
     _antVisual.gameObject.SetActive(false);
     _smoke.enabled = true;
-    _smoke.Play("Smoke",-1,0);
+    _smoke.Play(animState[UnityEngine.Random.Range(0,1)],-1,0);
     GetTimeToclear();
     _smoke.speed = 1;
     Blood.Play();
